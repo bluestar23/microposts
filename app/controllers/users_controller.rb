bd632aa3414e9b67f4d2779549b59dae3f1b3b2a
bluestar23 <c.morihira@hotmail.co.jp>
2016-04-29 12:33:19 +0000

    reuser-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :set_user, only: [:edit, :update, :destroy]
+
   def show 
    @user = User.find(params[:id])
   end
@@ -6,7 +9,6 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
 
   def create
     @user = User.new(user_params)
@@ -18,10 +20,30 @@ class UsersController < ApplicationController
     end
   end
 
+  #追加しました。by suzuki
+  def edit
+  end
+  
+  #追加しました。by suzuki
+  def update
+    if @user.update(user_params)
+      #保存に成功した場合はトップページへリダイレクト
+      render 'show' , notice: 'メッセージを編集しました'
+    else
+      #保存に失敗した場合は編集画面に戻す
+      render 'edit'
+    end
+  end  
+
   private
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :address, :profile,
                                  :password_confirmation)
   end
+
+  def set_user
+     @user = User.find(params[:id])
+  end
+
 end
